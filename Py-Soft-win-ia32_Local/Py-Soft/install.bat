3bcc3813ee391848c6c03b93b42e15e8ea682436
RMingov <riste.mingov@gmail.com>
2015-08-26 15:22:17 +0200

    local zip

--- Py-Soft-win-ia32_Local/Py-Soft/install.bat
+++ Py-Soft-win-ia32_Local/Py-Soft/install.bat
@@ -9,7 +9,7 @@
 set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 
 echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
-echo sLinkFile = "%USERPROFILE%\Desktop\Py-Soft.lnk" >> %SCRIPT%
+echo sLinkFile = "%USERPROFILE%\Desktop\Py-Soft_Local.lnk" >> %SCRIPT%
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
 echo oLink.TargetPath = "%CD%\Py-Soft.bat" >> %SCRIPT%
 echo oLink.Save >> %SCRIPT%
