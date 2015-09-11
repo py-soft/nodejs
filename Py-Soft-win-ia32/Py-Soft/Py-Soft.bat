2066bdc6a282b5f26fca65255f00c66984f70930
Jeremy Hoyle <jeremiah.r.hoyle@gmail.com>
2015-09-11 06:23:30 -0500

    Changing to port 62525

--- Py-Soft-win-ia32/Py-Soft/Py-Soft.bat
+++ Py-Soft-win-ia32/Py-Soft/Py-Soft.bat
@@ -1,1 +1,1 @@
-start "" C:\Users\Riste\Desktop\NODE-WEBKIT\Update\Py-Soft-win-ia32\Py-Soft\..\nwjs-v0.12.2-win-ia32\nw.exe C:\Users\Riste\Desktop\NODE-WEBKIT\Update\Py-Soft-win-ia32\Py-Soft\..\Py-Soft
+start "" C:\Users\Jeremy\nodejs\Py-Soft-win-ia32\Py-Soft\..\nwjs-v0.12.2-win-ia32\nw.exe C:\Users\Jeremy\nodejs\Py-Soft-win-ia32\Py-Soft\..\Py-Soft
