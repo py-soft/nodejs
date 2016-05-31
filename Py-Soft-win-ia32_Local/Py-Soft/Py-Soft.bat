47ad09a6658f6e7151cc495ebbc52e1fa2c72acc
jasmin.1m <jasmin.1m@hotmail.com>
2016-05-31 14:24:47 +0200

    x32 and x64 APPs

--- Py-Soft-win-ia32_Local/Py-Soft/Py-Soft.bat
+++
@@ -1,1 +0,0 @@
-start "" D:\pysoftwares\nodejs\Py-Soft-win-ia32_Local\Py-Soft\..\nwjs-v0.12.2-win-ia32\nw.exe D:\pysoftwares\nodejs\Py-Soft-win-ia32_Local\Py-Soft\..\Py-Soft
--- Py-Soft-win-x64/Py-Soft/Py-Soft.bat
+++
@@ -1,1 +0,0 @@
-start "" C:\Users\Riste\Desktop\NODE-WEBKIT\Update\Py-Soft-win-x64\Py-Soft\..\nw\nw.exe C:\Users\Riste\Desktop\NODE-WEBKIT\Update\Py-Soft-win-x64\Py-Soft\..\Py-Soft
